8373743621dbfd1c3b76f364e2864c537b76eed7
Øyvind <oyvind.w.petersen@ntnu.no>
2023-03-28 20:33:04 +0200

    minor

--- checkxaxis.m
+++ checkxaxis.m
@@ -1,6 +1,6 @@
 function isxaxis=checkxaxis(a)
-
-%% Check if argument in is a time axis
+%% Check if argument in is a vectorized axis, e.g. time or frequency
+% It is assumed elements of the vector is in increasing order
 %
 % Inputs:
 % a: vector with x-axis, e.g. time vector
@@ -31,11 +31,6 @@ if ~isa(a,'double')
     a=double(a);
 end
 
-% if any(diff(a)<=0)
-%     isxaxis=false;
-%     return
-% end
-
 if sum(isnan(a))>1
 	isxaxis=false;
     return
@@ -46,42 +41,12 @@ if any(any(any(imag(a))))
     return
 end
 
-
 n=length(a);
 
 n_div=min(100,n);
 ind_check=ceil(linspace(1,n,n_div));
 
-if any(diff(a(ind_check))<0)
+if any(diff(a(ind_check))<=0)
 	isxaxis=false;
     return
 end    
-% n_window=min(100,length(a));
-% n_check=10;
-% 
-% indCheck=floor(linspace(0,max(n_window,length(a)-n_window),n_check+1));
-% indCheck=indCheck(1:end-1);
-
-% indRemove=(indCheck+n_check)>length(a);
-% indCheck(indRemove)=[];
-
-% for k=1:length(indCheck)
-    
-%     indexRangeCheck=indCheck(k)+[1:n_window];
-    
-
-    
-%     if max(indexRangeCheck)>length(a)
-%     break; return
-%     end        
-    
-%     ratio=std(diff(a(indexRangeCheck)))./median(diff(a(indexRangeCheck)));
-% 	if ratio>1e-6
-% % 	isXaxis=false;
-% %     break; return
-%     end
-%     
-% end
-    
-% end
-
