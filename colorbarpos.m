9f1affd100582eede537f320cdede801715e881d
Øyvind <oyvind.w.petersen@ntnu.no>
2023-08-16 21:47:54 +0200

    dx,dy
    For translation

--- colorbarpos.m
+++ colorbarpos.m
@@ -1,4 +1,4 @@
-function colorbarpos(hc,scale_x,scale_y)
+function colorbarpos(hc,scale_x,scale_y,dx,dy)
 
 %% Scale down size of color bar
 %
@@ -6,22 +6,36 @@ function colorbarpos(hc,scale_x,scale_y)
 % hc: handle to color bar
 % scale_x: rescale factor, between 0 and 1
 % scale_y: rescale factor, between 0 and 1
+% dx: adjust x
+% dy: adjust y
 %
 % Outputs:
 %
 %%
 
+if nargin<5
+    dy=0;
+end
+
+if nargin<4
+    dx=0;
+end
+
+
 ax=1-scale_x;
 ay=1-scale_y;
 
 Temp=hc.Position;
-% [start_x start_y dx dy];
+% [start_x start_y Lx Ly];
 
 start_x=Temp(1);
 start_y=Temp(2);
-dx=Temp(3);
-dy=Temp(4);
+Lx=Temp(3);
+Ly=Temp(4);
 
-Temp2=[start_x+ax*dx start_y+ay*dy dx-ax*dx dy-ay*dy];
+Temp2=[ start_x+ax*Lx+dx ...
+        start_y+ay*Ly+dy ...
+        Lx-ax*Lx  ...
+        Ly-ay*Ly];
 
 hc.Position=Temp2;
