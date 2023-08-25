7466298923d09dd9e0d7ee918064cabe69a87fe6
Øyvind <oyvind.w.petersen@ntnu.no>
2023-08-25 11:14:11 +0200

    doc, rename

--- surfiso.m
+++ surfiso.m
@@ -1,5 +1,15 @@
 function [h_surf,h_iso]=surfiso(x,f,varargin)
 
+%% 2D surface with isolines
+%
+% Inputs:
+% x: [N,2] matrix with x1 and x2 values as columns
+% f: [N,1] vector with surface values
+%
+% Outputs:
+% h_surf: handle to surface
+% h_iso: handle to isolines
+%
 %%
 
 p=inputParser;
@@ -7,17 +17,19 @@ p=inputParser;
 addParameter(p,'xlabel','x',@ischar)
 addParameter(p,'ylabel','y',@ischar)
 addParameter(p,'zlabel','z',@ischar)
+addParameter(p,'facealpha',0.8,@isnumeric)
 addParameter(p,'displayname','',@ischar)
-addParameter(p,'isoval',[20],@isnumeric) % Number of isolines between [f_min,f_max], or vector with values
-addParameter(p,'view',[40 15],@isnumeric) % Number of isolines between [f_min,f_max], or vector with values
+addParameter(p,'isolines',[20],@isnumeric) % Number of isolines between [f_min,f_max]
+addParameter(p,'view',[40 15],@isnumeric)
 
 parse(p,varargin{1:end});
 
 xlab=p.Results.xlabel;
 ylab=p.Results.ylabel;
 zlab=p.Results.zlabel;
+facealpha=p.Results.facealpha;
 displayname=p.Results.displayname;
-isoval=p.Results.isoval;
+isolines=p.Results.isolines;
 viewvec=p.Results.view;
 
 %%
@@ -26,19 +38,20 @@ if size(x,2)~=2
     error('x must be 2 columns');
 end
 
-if size(x,1)~=size(f,1)
-    error('x and f must have equal rows');
-end
+f=f(:);
 
+if size(x,1)~=size(f,1)
+    error('x and f must have equal number of rows');
+end
 
 % Surf
 tri=delaunay(x(:,1),x(:,2));
-h_surf=trisurf(tri,x(:,1),x(:,2),f,'EdgeColor','none','FaceAlpha','0.8','DisplayName',displayname);
+h_surf=trisurf(tri,x(:,1),x(:,2),f,'EdgeColor','none','FaceAlpha',facealpha,'DisplayName',displayname);
 shading interp
 
 % Isoline
-SurfCell={tri,[x(:,1),x(:,2),f]};
-[h_iso,V]=IsoLine(SurfCell,f,isoval,[0.5 0.5 0.5]);
+surfcell={tri,[x(:,1),x(:,2),f]};
+[h_iso,V]=IsoLine(surfcell,f,isolines,[0.5 0.5 0.5]);
 
 colormap(brewermap(100,'GnBu'));
 hc=colorbar('Location','north');
