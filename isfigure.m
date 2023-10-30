7f1ae077a4c30eaf7b2b9b8cc91ef1fb3d30ffe9
Øyvind <oyvind.w.petersen@ntnu.no>
2023-10-30 10:00:42 +0100

    doc

--- isfigure.m
+++ isfigure.m
@@ -1,7 +1,16 @@
 function logic = isfigure(h_handle)
+%% Check if figure
+%
+% Inputs:
+% h_handle: query handle
+%
+% Outputs:
+% logic: true/false 
+%
+%% 
+
 try
     logic = strcmp(get(h_handle, 'type'), 'figure');
 catch
     logic = false;
-end
 end
