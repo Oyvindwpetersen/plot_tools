7b526fffa43f84daf440519df140d6cb266344b5
Øyvind <oyvind.w.petersen@ntnu.no>
2023-11-30 10:50:11 +0100

    optional ) or ()

--- main/tight_subplot_letter.m
+++ main/tight_subplot_letter.m
@@ -1,4 +1,4 @@
-function tight_subplot_letter(ha,fontsize,p_in)
+function tight_subplot_letter(ha,fontsize,p_in,prepost)
 
 %% Add letter label to axes
 %
@@ -9,14 +9,18 @@ function tight_subplot_letter(ha,fontsize,p_in)
 %
 % Outputs:
 %
-%% 
+%%
 
-if nargin==1
-    fontsize=6; p_in=[];
+if nargin<2
+    fontsize=6;
 end
 
-if nargin==2
+if nargin<3
     p_in=[];
+end
+
+if nargin<4
+    prepost=')';
 end
 
 if isfigure(ha)
@@ -24,67 +28,76 @@ if isfigure(ha)
 end
 
 if ~iscell(ha)
-	ha={ha};
+    ha={ha};
+end
+
+s={'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm'};
+
+for k=1:length(s)
+
+    if strcmpi(prepost,')')
+        s{k}=[s{k} ')'];
+            elseif strcmpi(prepost,'()')
+        s{k}=['(' s{k} ')'];
+            end
 end
 
 for j=1:length(ha)
 
-s={'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm'};
+    if length(ha{j})==1
+        continue
+    end
 
-if length(ha{j})==1
-	continue
-end
+    clear p_all
+    for k=1:length(ha{j})
+        p_all(k,:)=get(ha{j}(k),'Position');
+    end
 
-clear p_all
-for k=1:length(ha{j})
-	p_all(k,:)=get(ha{j}(k),'Position');
-end
+    % coord_BL_X=p_all(:,1); coord_TL_X=p_all(:,1)+p_all(:,3);
+    coord_BL_Y=p_all(:,2); coord_TL_Y=p_all(:,2)+p_all(:,4);
 
-% coord_BL_X=p_all(:,1); coord_TL_X=p_all(:,1)+p_all(:,3);
-coord_BL_Y=p_all(:,2); coord_TL_Y=p_all(:,2)+p_all(:,4);
+    for k=1:length(ha{j})
+        DeltaY_k(k)=min(abs(coord_BL_Y(k)-coord_TL_Y));
+    end
 
-for k=1:length(ha{j})
-	DeltaY_k(k)=min(abs(coord_BL_Y(k)-coord_TL_Y));
-end
+    DeltaY=mean(DeltaY_k);
 
-DeltaY=mean(DeltaY_k);
+    if DeltaY>0.5; DeltaY=0.1; end
 
-if DeltaY>0.5; DeltaY=0.1; end
+    for k=1:length(ha{j})
 
-for k=1:length(ha{j})
+        % 	p=get(ha{j}(k),'Position');
 
-% 	p=get(ha{j}(k),'Position');
+        p=p_all(k,:);
 
-    p=p_all(k,:);
+        if isempty(p_in)
+            p2(1)=p(1)-DeltaY*0.3;
+            p2(2)=p(2)-DeltaY*0.4;
+            p2(3)=0.05;
+            p2(4)=0.05;
+        else
+            p2(1)=p(1)+p_in(1);
+            p2(2)=p(2)+p_in(2);
+            p2(3)=0.05;
+            p2(4)=0.05;
+        end
 
-    if isempty(p_in)
-    p2(1)=p(1)-DeltaY*0.3;
-    p2(2)=p(2)-DeltaY*0.4;
-    p2(3)=0.05;
-    p2(4)=0.05;        
-    else
-    p2(1)=p(1)+p_in(1);
-    p2(2)=p(2)+p_in(2);
-    p2(3)=0.05;
-    p2(4)=0.05;
-	end
-    
-	p2(p2<0)=0.01;
-		
-	a=annotation(gcf,'textbox',...
-    p2,...
-    'String',{[s{k} ')']},...
-    'FitBoxToText','off',...
-    'EdgeColor','none',...
-    'Color',[0 0 0],...
-	'FontSize',fontsize,'FontName','Arial'...
-	);
-end
+        p2(p2<0)=0.01;
+
+        a=annotation(gcf,'textbox',...
+            p2,...
+            'String',s{k},...
+            'FitBoxToText','off',...
+            'EdgeColor','none',...
+            'Color',[0 0 0],...
+            'FontSize',fontsize,'FontName','Arial'...
+            );
+    end
 end
 
 %%
 
-	
+
 %     p2(1)=p(1)-p_in(1);
 %     p2(2)=p(2)-p_in(2);
 %     p2(3)=p_in(3);
@@ -94,5 +107,5 @@ end
 % % 	p2(2)=p(2)-p(4)*0.2;
 %     p2(2)=p(2)-0.075;
 % 	p2(3)=0.05;
-% 	p2(4)=0.05;                 
+% 	p2(4)=0.05;
 %     end
