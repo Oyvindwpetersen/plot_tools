f69927180b43bd465e114c662744d43d2b4c40b5
Øyvind <oyvind.w.petersen@ntnu.no>
2024-04-25 15:53:06 +0200

    label num or string

--- main/plot_time_freq/labelgrid.m
+++ main/plot_time_freq/labelgrid.m
@@ -22,8 +22,8 @@ function ylabel=labelgrid(n1,n2,letter,prefix,postfix)
 % Note ylabel is row-by-row of y_3d, same as plotting order
 %
 % Inputs:
-% n1: size of y in dim1
-% n2: size of y in dim2
+% n1: size of y in dim1 or cell with labels
+% n2: size of y in dim2 or cell with labels
 % letter: letter, e.g. 'H'
 % prefix: character before letter
 % postfix: character after ij
@@ -45,16 +45,41 @@ if nargin<3
     letter='H';
 end
 
-
 %% Generate labels
 
-n=0;
-ylabel={};
-for k1=1:n1
-    for k2=1:n2
-        n=n+1;
-        ylabel{k1,k2}=['$' prefix letter '_{' num2str(k1) num2str(k2) '}' postfix '$'];
+if isnumeric(n1) & isnumeric(n2)
+
+    n1_tmp=[1:n1];
+    n1={};
+    for k1=1:length(n1_tmp)
+        n1{k1}=num2str(n1_tmp(k1));
     end
+
+    n2_tmp=[1:n2]
+    n2={};
+    for k2=1:length(n2_tmp)
+        n2{k2}=num2str(n2_tmp(k2));
+    end
+
+elseif iscell(n1) | iscell(n2)
+    
+    if ischar(n1)
+        n1={n1};
+    end
+
+    if ischar(n2)
+        n2={n2};
+    end
+
 end
-ylabel=permute(ylabel,[2 1]); ylabel=ylabel(:);
+
+ylabel={};
+for k1=1:length(n1)
+        for k2=1:length(n2)
+            ylabel{k1,k2}=['$' prefix letter '_{' n1{k1}  n2{k2} '}' postfix '$'];
+        end
+    end
+    ylabel=permute(ylabel,[2 1]); ylabel=ylabel(:);
+
+end
 
