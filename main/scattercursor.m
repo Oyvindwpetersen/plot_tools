61882577ec92b291a83c18c9c036df4e262d3d14
Øyvind <oyvind.w.petersen@ntnu.no>
2024-05-30 20:28:32 +0200

    working version

--- main/scattercursor.m
+++ main/scattercursor.m
@@ -23,6 +23,8 @@ end
 if sync
     hax=getsortedaxes(gcf);
 
+    ind_del=[];
+
     for k=1:length(hax)
         hax_k=get(hax(k));
         
@@ -37,11 +39,15 @@ if sync
             childScatter=hax_k.Children;
         end
                 
-        if isempty(childScatter); continue; end
+        if isempty(childScatter)
+            ind_del(end+1)=k;
+            continue; 
+        end
 
         data_x_all(k,:)=childScatter.XData(1,:);
         data_y_all(k,:)=childScatter.YData(1,:);
         data_z_all(k,:)=childScatter.ZData(1,:);
+        
     end
 else
 	hax=[];
@@ -54,13 +60,16 @@ dcm=datacursormode(gcf);
 set(dcm,'Interpreter','none');
 
 datacursormode on
-set(dcm,'updatefcn',{@infoCursor hax sync cell_string data_x_all data_y_all data_z_all});
+
+% hax(ind_del)=[];
+
+set(dcm,'updatefcn',{@infoCursor hax ind_del sync cell_string data_x_all data_y_all data_z_all});
 
 
 end
 %%
 
-function [output_txt] = infoCursor(obj,event_obj,hax,sync,cell_string,data_x_all,data_y_all,data_z_all)
+function [output_txt] = infoCursor(obj,event_obj,hax,ind_del,sync,cell_string,data_x_all,data_y_all,data_z_all)
 % Display the position of the data cursor
 % obj          Currently not used (empty)
 % event_obj    Handle to event object
@@ -81,44 +90,102 @@ err_y=data_y-y;
 
 [~,indMin]=min(abs(err_x./max(abs(err_x)))+abs(err_y./max(abs(err_y))));
 
+% if ~isempty(cell_string)
+%     output_txt{end+1}=cell_string{indMin};
+% end
+
 output_txt= {...    
     ['x = ' num2str(data_x(indMin),'%.3e')],...
     ['y = ' num2str(data_y(indMin),'%.3e')],...
     ['index = ' num2str(indMin,'%0.0f')],...
+    ['str = ' cell_string{indMin}],...
     };
 
-
-if ~isempty(cell_string)
-    output_txt{end+1}=cell_string{indMin};
-end
-
-if sync
+if 1
     
     hax_current=event_obj.Target.Parent;
     
     for k=1:length(hax)
+
+        if any(k==ind_del)
+            continue
+        end
         
 %         axes(hax(k));
         hLine=findobj(hax(k),'Type','Line');
-        
-        for j=1:length(hLine)
-            if strcmpi(hLine(j).Tag,'extra')
-                delete(hLine(j));
+
+            if ~isempty(hLine)
+            
+            for j=1:length(hLine)
+                if strcmpi(hLine(j).Tag,'extra')
+                    delete(hLine(j));
+                end
             end
+            
+            clear hLine;
+
         end
         
-        clear hLine;
+        if hax(k)==hax_current
+            MarkerEdgeColor=[0 1 1]; MarkerSize=10; 
+        else
+            MarkerEdgeColor=[1 0.5 0]; MarkerSize=10;
+        end
+
+        hp=plot(data_x_all(k,indMin),data_y_all(k,indMin),...
+            'o','MarkerSize',MarkerSize,...
+            'MarkerEdgeColor',MarkerEdgeColor,'LineWidth',2,'Parent',hax(k));
         
-%         if hax(k)==hax_current; MarkerEdgeColor=[0 1 1]; MarkerSize=10; else; MarkerEdgeColor=[0.5 0.5 1]; MarkerSize=10; end
-        if hax(k)==hax_current; MarkerEdgeColor=[0 1 1]; MarkerSize=10; else; MarkerEdgeColor=[1 0.5 0]; MarkerSize=10; end
-%         axes(hax(k));
-        hLine(k)=plot(data_x_all(k,indMin),data_y_all(k,indMin),'o','MarkerSize',MarkerSize,'MarkerEdgeColor',MarkerEdgeColor,'LineWidth',2,'Parent',hax(k));
-        hLine(k).Tag='extra';
+        hp.Tag='extra';
         
     end
     %    delete(hLine)
     
 end
+
+
+% %%
+% if 0
+% 
+%  hax_current=event_obj.Target.Parent;
+% 
+%     for k=1:length(hax)
+% 
+%            hLine=findobj(hax(k),'Type','Line');
+% 
+%         for j=1:length(hLine)
+%             if strcmpi(hLine(j).Tag,'extra')
+%                 delete(hLine(j));
+%             end
+%         end
+% 
+%         clear hLine;
+% 
+%                 % if hax(k)==hax_current
+%             MarkerEdgeColor=[0 1 1]; MarkerSize=10; 
+%         % else
+%             % MarkerEdgeColor=[1 0.5 0]; MarkerSize=10;
+%         % end
+% 
+%         % axes(hax(k));
+% 
+%         % plot(data_x_all(k,indMin),data_y_all(k,indMin),'o','Parent',hax(k));
+% 
+%         hp=plot(data_x_all(k,indMin),data_y_all(k,indMin),...
+%             'o','MarkerSize',MarkerSize,...
+%             'MarkerEdgeColor',MarkerEdgeColor,'LineWidth',2,'Parent',hax(k));
+% 
+%         hp.Tag='extra';
+% 
+% 
+%     end
+% 
+%     % axes(hax_current);
+% end
+
+
+
+
 
 end
 
