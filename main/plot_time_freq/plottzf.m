b985bf814bb689424dd44f31ea083bfe7b740aba
Øyvind <oyvind.w.petersen@ntnu.no>
2025-02-19 15:03:27 +0100

    tzf

--- main/plot_time_freq/plottzf.m
+++ main/plot_time_freq/plottzf.m
@@ -28,8 +28,12 @@ if isstruct(args_in{end})
         plotopt=rmfield(plotopt,'weight');
     end
 
-end
+    if isfield(plotopt,'unit')
+        unit=plotopt.unit;
+        plotopt=rmfield(plotopt,'unit');
+    end
 
+end
 
 plotopt_td=plotopt;
 plotopt_fd=plotopt;
@@ -39,9 +43,17 @@ plotopt_td.ylabel=labelgrid(plotopt.ylabel,'(t)','');
 
 plotopt_fd.xlim=xlim2;
 plotopt_fd.log=log_;
+plotopt_fd.unit=unit;
 
-plotopt_fd.unit='rad/s';
-plotopt_fd.ylabel=labelgrid(plotopt.ylabel,'(\omega)','');
+if strcmpi(plotopt_fd.unit,'hz')
+    f='(f)';
+elseif strcmpi(plotopt_fd.unit,'rad/s')
+    f='(\omega)';
+end
+
+for idx=1:length(plotopt.ylabel)
+    plotopt_fd.ylabel{idx}= ['$|' plotopt.ylabel{idx} f '|$' ];
+end
 
 args_in_td=args_in;
 args_in_td{end}=plotopt_td;
@@ -49,30 +61,36 @@ args_in_td{end}=plotopt_td;
 args_in_fd=args_in;
 args_in_fd{end}=plotopt_fd;
 
-
 %%
 
-ha=plottimezoom(args_in_td{:});
+ha_td=plottimezoom(args_in_td{:});
 
-[~,pos]=tight_subplot(length(ha)/2,3,plotopt_fd.gap,...
+[~,pos]=tight_subplot(length(ha_td)/2,3,plotopt_fd.gap,...
     plotopt_fd.marg_h,plotopt_fd.marg_w,[],weight,[],true);
 
 idx=-1;
 for k=1:3:length(pos)
     idx=idx+2;
-    set(ha(idx),'Position',pos{k});
-    set(ha(idx+1),'Position',pos{k+1});
+    set(ha_td(idx),'Position',pos{k}); % Set pos time series
+    set(ha_td(idx+1),'Position',pos{k+1}); % Set pos zoom
 end
 
-idx=0;
+clear ha
+ha(1:3:length(pos))=ha_td(1:2:end);
+ha(2:3:length(pos))=ha_td(2:2:end);
+
 for k=3:3:length(pos)
-    idx=idx+1;
-    ha_new(idx)=axes('Units','normalized','Position',pos{k});
+    ha(k)=axes('Units','normalized','Position',pos{k});
 end
 
-% args_in_fd{1}=args_in_fd{1}*2*pi;
-ha_tmp=plotfreq(args_in_fd{:});
+ha_fd=plotfreq(args_in_fd{:});
 
-copyaxescontent(ha_tmp,ha_new,true,false);
+copyaxescontent(ha_fd,ha(3:3:end),true,false);
 
-axislabelmulti(gcf,{'$t$ [s]' '$t$ [s]' '$\omega$ [rad/s]'},'bottom');
+if strcmpi(plotopt_fd.unit,'hz')
+    label_f='$f$ [Hz]';
+elseif strcmpi(plotopt_fd.unit,'rad/s')
+    label_f='$\omega$ [rad/s]';
+end
+
+axislabelmulti(gcf,{'$t$ [s]' '$t$ [s]' label_f},'bottom');
